9f603f87f300155545af3a6b1f845d62a509f600
Ram <131847922+Rambhai567@users.noreply.github.com>
2023-09-25 00:44:16 +0530

    Add files via upload

--- public/js/client.js
+++ public/js/client.js
@@ -1,80 +1,96 @@
-const socket=io();
+const socket = io();
 
 var username;
-var chats=document.querySelector(".chats");
-var users_list=document.querySelector(".users-list");
-var users_count=document.querySelector(".users-count");
-var msg_send=document.querySelector("#user-send");
-var user_msg=document.querySelector("#user-msg");
+var chats = document.querySelector(".chats");
+var users_list = document.querySelector(".users-list");
+var users_count = document.querySelector(".users-count");
+var msg_send = document.querySelector("#user-send");
+var user_msg = document.querySelector("#user-msg");
 
+// Load stored messages from Local Storage
+function loadStoredMessages() {
+    const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+    storedMessages.forEach((data) => {
+        appendMessage(data, 'incoming');
+    });
+}
 
+do {
+    username = prompt("Enter your name: ");
+} while (!username);
 
+// Load stored messages when the page loads
+loadStoredMessages();
 
-do{
-    username=prompt("Enter your name: ");
-}while(!username);
+socket.emit("new-user-joined", username);
 
-
-socket.emit("new-user-joined",username);
-
-socket.on('user-connected',(socket_name)=>{
-    userJoinLeft(socket_name,'joined');
+socket.on('user-connected', (socket_name) => {
+    userJoinLeft(socket_name, 'joined');
 });
 
 // user is join or left
-function userJoinLeft(name,status){
-    let div=document.createElement("div");
+function userJoinLeft(name, status) {
+    let div = document.createElement("div");
     div.classList.add('user-join');
-    let content=`<p><b>${name}</b> ${status} the chat</p>`;
-    div.innerHTML=content;
+    let content = `<p><b>${name}</b> ${status} the chat</p>`;
+    div.innerHTML = content;
     chats.appendChild(div);
-};
+}
 
-socket.on('user-disconnected',(user)=>{
-    userJoinLeft(user,'left');
+socket.on('user-disconnected', (user) => {
+    userJoinLeft(user, 'left');
 });
 
-socket.on('user-list', (users)=>{
-    users_list.innerHTML="";
-    users_arr=Object.values(users);
-    for(i=0;i<users_arr.length;i++){
-        let p=document.createElement('p');
-        p.innerText=users_arr[i];
+socket.on('user-list', (users) => {
+    users_list.innerHTML = "";
+    users_arr = Object.values(users);
+    for (i = 0; i < users_arr.length; i++) {
+        let p = document.createElement('p');
+        p.innerText = users_arr[i];
         users_list.appendChild(p);
     }
-    users_count.innerHTML=users_arr.length;
+    users_count.innerHTML = users_arr.length;
 });
 
-
-
 // for sending msg
-
-msg_send.addEventListener('click', ()=>{
-    let data={
+msg_send.addEventListener('click', () => {
+    let data = {
         user: username,
         msg: user_msg.value
     };
-    if(user_msg.value!=''){
+    if (user_msg.value != '') {
         appendMessage(data, 'outgoing');
-        socket.emit('message',data);
-        user_msg.value='';
+        socket.emit('message', data);
+        user_msg.value = '';
+
+        // Store the message in Local Storage
+        const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+        storedMessages.push(data);
+        localStorage.setItem('chatMessages', JSON.stringify(storedMessages));
     }
 });
 
-
-function appendMessage(data,status){
-    let div=document.createElement('div');
+function appendMessage(data, status) {
+    let div = document.createElement('div');
     div.classList.add('message', status);
-    let content=`
+    let content = `
     <h5>${data.user}</h5>
     <p>${data.msg}</p>
     `;
-    div.innerHTML=content;
+    div.innerHTML = content;
     chats.appendChild(div);
-    chats.scrollTop=chats.scrollHeight;
+    chats.scrollTop = chats.scrollHeight;
 }
 
+socket.on('message', (data) => {
+    appendMessage(data, 'incoming');
+});
 
-socket.on('message',(data)=>{
-    appendMessage(data,'incoming');
-})
+const clearChatBtn = document.getElementById('clearChatBtn');
+clearChatBtn.addEventListener('click', () => {
+    // Clear chat messages from the chat display locally
+    chats.innerHTML = '';
+
+    // Clear chat messages from Local Storage locally
+    localStorage.removeItem('chatMessages');
+});
