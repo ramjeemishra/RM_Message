1508c52420e4efd9ebbcc0fe11e56a6ec94aa953
Ramjee Mishra <131847922+ramjeemishra@users.noreply.github.com>
2024-07-19 22:37:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const http = require("http");
 const express = require("express");
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 4000;
@@ -18,24 +14,7 @@ app.get('/', (req, res) => {
 const io = require("socket.io")(server);
 let users = {};
 
-// Configure multer for file uploads
-const upload = multer({ dest: 'public/uploads/' });
 
-// Handle image uploads
-app.post('/upload', upload.single('image'), (req, res) => {
-  const tempPath = req.file.path;
-  const targetPath = path.join(__dirname, 'public/uploads/', req.file.originalname);
-
-  fs.rename(tempPath, targetPath, err => {
-    if (err) return res.sendStatus(500);
-
-    const imageUrl = `/uploads/${req.file.originalname}`;
-    res.json({ imageUrl });
-
-    // Broadcast the image URL to all connected clients
-    io.emit('imageMessage', { sender: req.body.username, imageUrl });
-  });
-});
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (username) => {
